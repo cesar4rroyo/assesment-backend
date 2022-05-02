50c651cb2b503baca870cd899324e105c68485ac
Cesar Arroyo <cesardavid_09@hotmail.com>
2022-05-01 21:00:57 -0500

    feat: adding services

---
+++ src/services/fav.service.js
@@ -0,0 +1,38 @@
+import Fav from "../models/fav.model"
+
+export async function createFavService(req) {
+    const {title, description, link} = req.body
+    const fav = await Fav.create(
+        {
+            title, 
+            description, 
+            link
+        }
+    )
+    return fav
+}
+
+export async function getFavsService(req) {
+    const favs = await Fav.find()
+    return favs
+}
+
+export async function getFavByIdService(req) {
+    const fav = await Fav.findById(req.params.id)
+    return fav
+}
+
+export async function updateFavByIdService(req) {
+    const {title, description, link} = req.body
+    const fav = await Fav.findByIdAndUpdate(req.params.id, {
+        title, 
+        description, 
+        link
+    }, {new: true})
+    return fav
+}
+
+export async function deleteFavByIdService(req) {
+    const fav = await Fav.findByIdAndDelete(req.params.id)
+    return fav
+}
---
+++ src/services/listFav.service.js
@@ -0,0 +1,30 @@
+import ListFav from "../models/listFav.model"
+
+
+export async function createListFavService(req) {
+    const listFav = await ListFav.create(req.body)
+    return listFav.populate("user").populate("favs")
+}
+
+export async function getListFavsService(req) {
+    const listFavs = await ListFav.find()
+    listFavs.forEach(listFav => {
+        await listFav.populate("user").populate("favs")
+    })
+    return listFavs
+}
+
+export async function getListFavByIdService(req) {
+    const listFav = await ListFav.findById(req.params.id)
+    return await listFav.populate("user").populate("favs")
+}
+
+export async function updateListFavByIdService(req) {
+    const listFav = await ListFav.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    return await listFav.populate("user").populate("favs")
+}
+
+export async function deleteListFavByIdService(req) {
+    const listFav = await ListFav.findByIdAndDelete(req.params.id)
+    return []
+}
